a2e6feeba9013f52041a150a9d2deb4448431a4c
Sakwa Alvitre <sakwaa@gmail.com>
2015-01-31 20:40:34 -0800

    removing quote cost functionality

--- RoomCarpet.java
+++ RoomCarpet.java
@@ -10,6 +10,6 @@ public class RoomCarpet {
         this.carpetCost = cost;
     }
     protected double getTotalCost() {
-        return this.roomDimension.getArea() * this.carpetCost;
+        return 0; //this.roomDimension.getArea() * this.carpetCost;
     }
 }
